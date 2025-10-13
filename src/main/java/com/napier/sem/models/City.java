bf8dcd85b9dce8700ed9e8a5eed4f85e9fd73171
marko-romaniuk <markoromaniuk90@gmail.com>
2025-10-13 23:28:29 +0100

    Add city sorting strings

---
+++ src/main/java/com/napier/sem/models/City.java
@@ -0,0 +1,31 @@
+package com.napier.sem.models;
+
+/**
+ * Represents a city.
+ */
+public class City {
+    /**
+     * City's ID (e.g., 1 , 10 , 67).
+     */
+    public String id;
+
+    /**
+     * City's name (e.g., 'Kyiv).
+     */
+    public String name;
+
+    /**
+     * Country where the city is located.
+     */
+    public String code;
+
+    /**
+     * District where the city is located.
+     */
+    public String district;
+
+    /**
+     * The population of the city.
+     */
+    public int population;
+}
